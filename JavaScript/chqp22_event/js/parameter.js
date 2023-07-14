9bcbe07e2ea9f64493a63250462fe83f57628e25
songyunjeong <yungunaa@gmail.com>
2023-07-14 20:40:12 +0900

    day19 JavaScript 혼자 첵실습 & event

---
+++ JavaScript/chqp22_event/js/parameter.js
@@ -0,0 +1,45 @@
+function getParameter(paramName, paramValue) {
+  var parameter = {
+    paramName: paramName,
+    paramValue: paramValue,
+  };
+
+  return parameter;
+}
+
+function getParameters(search) {
+  var params = search.substr(1).split("&"); // substr(1): 물음표 제거, 물음표 빼고 다음 문자열부터 가져옴
+
+  for (var i in params) {
+    var param = params[i].split("=");
+    params[i] = getParameter(param[0], decodeURIComponent(param[1]));
+  }
+
+  var parameters = {
+    params: params,
+
+    getParamValue: function (paramName) {
+      var paramValue = "";
+
+      for (var i in this.params) {
+        if (params[i].paramName === paramName) {
+          paramValue = params[i].paramValue;
+          break;
+        }
+      }
+
+      return paramValue;
+    },
+  };
+
+  return parameters;
+}
+
+/*
+var search = "?name=harper&age=28&gender=female";
+alert(
+  getParameters(search)[1].paramName +
+    " : " +
+    getParameters(search)[1].paramValue
+);
+*/
